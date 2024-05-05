972592baad0a037e74c5cd7e4c51082abe3ae999
iamtonmoy0 <tonmoy2code@gmail.com>
2024-05-05 14:59:06 +0600

    Add @radix-ui/react-toast, @types/bcryptjs, axios, usehooks-ts libraries
    Update package-lock.json and package.json
    
    Update src/app/(auth)/sign-

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,17 @@
-import React from "react";
-
+"use client";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import { useDebounceValue } from "usehooks-ts";
 function page() {
+  const [username, setUsername] = useState("");
+  const [usernameMessage, setUsernameMessage] = useState("");
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const debounced = useDebounceValue(username, 300);
+  const { toast } = useToast();
+  const router = useRouter();
   return <div>page</div>;
 }
 
