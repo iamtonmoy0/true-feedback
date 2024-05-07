77576415375f3d5b5e8e5a0160f18e0eb21fd242
iamtonmoy0 <tonmoy2code@gmail.com>
2024-05-08 00:08:55 +0600

    "Add zod validation, form handling, and API calls for sign-up page"
    This commit adds zod validation, form handling using react-hook-form, and API calls for checking username uniqueness and submitting sign-up

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 import { useToast } from "@/components/ui/use-toast";
+import { signupSchema } from "@/schemas/signUpSchema";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import axios, { AxiosError } from "axios";
 import { useDebounceValue } from "usehooks-ts";
+import { z } from "zod";
 
 function page() {
   const [username, setUsername] = useState("");
@@ -13,6 +17,50 @@ function page() {
   const debounced = useDebounceValue(username, 300);
   const { toast } = useToast();
   const router = useRouter();
+  // zod implementation
+  const form = useForm<z.infer<typeof signupSchema>>({
+    resolver: zodResolver(signupSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  useEffect(() => {
+    const checkUsername = async () => {
+      if (debounced) {
+        setIsCheckingUsername(true);
+        setUsernameMessage("");
+        try {
+          const response = await axios.get(
+            `/api/check-username-unique?username=${debounced}`
+          );
+          console.log(response);
+          setUsernameMessage(response.data.message);
+        } catch (error) {
+          const axiosError = error as AxiosError;
+          setUsernameMessage("failed to check unique username");
+        } finally {
+          setIsCheckingUsername(false);
+        }
+      }
+    };
+    checkUsername();
+  }, [debounced]);
+
+  // handle submit
+  const handleSubmit = async (data: z.infer<typeof signupSchema>) => {
+    setIsSubmitting(true);
+    try {
+      const res = await axios.post("/api/sign-up", data);
+      toast({
+        title: "success",
+        description: res.data.message,
+      });
+      router.replace(`/verify/${username}`);
+    } catch (error) {}
+  };
   return <div>page</div>;
 }
 
